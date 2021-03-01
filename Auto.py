08c985675d960a0770f66d3be24855ce4f857360
ChangMQ267 <ChangMQ267@163.com>
2021-03-01 20:17:39 +0800

    修改TDS文件名称,修改温度控制为最小值和最大值

--- Auto.py
+++ Auto.py
@@ -6,17 +6,20 @@ import gpio_init as gpio
 import Temperature as temp
 import time
 
-def tempauto(setTemp):
+
+def tempauto(MinTemp, MaxTemp):
     # GPIO.setmode(GPIO.BOARD)
     while True:
         nowTemp = temp.readTemp()
-        print("当前温度为:%f" % nowTemp, "设定温度为:%f" % setTemp)
-        if nowTemp < setTemp:
+        print("当前温度为:%.3f" % nowTemp, "设定最低温度为:%.3f" % MinTemp, "设定最高温度为:%.3f" % MaxTemp)
+        if nowTemp < MinTemp:
             gpio.add_temp()
             print("开启加热")
-        else:
+        elif nowTemp > MaxTemp:
             gpio.stop_add_temp()
             print("停止加热")
+        else:
+            pass
         time.sleep(5)
 
 
@@ -24,4 +27,4 @@ if __name__ == '__main__':
     gpio.init()
     gpio.add_oxy()
     gpio.water_start()
-    tempauto(22.5)
+    tempauto(22.5, 23)
