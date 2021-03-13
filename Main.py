8d798969be7576d819c14d3f900a703bc1ef46ce
ChangMQ267 <ChangMQ267@163.com>
2021-03-13 19:49:39 +0800

    新增数据库操作

--- Main.py
+++ Main.py
@@ -3,9 +3,9 @@ import time
 
 import gpio_init
 import TempAuto
-import Temperature
 import TDS
 import video
+from dbUtils import water_status_db
 from led_auto import ledauto
 
 MinTemp = 22.9
@@ -13,14 +13,19 @@ MaxTemp = 23
 SetHourStart = 8
 SetMinuteStart = 0
 SetHourEnd = 22
-SetMinuteEnd= 30
+SetMinuteEnd = 30
 
 if __name__ == '__main__':
     gpio_init.init()
     #gpio_init.add_oxy()
-   # video.video_start()
+    video.video_start()
+
     while True:
-        ledauto(SetHourStart,SetMinuteStart,SetHourEnd,SetMinuteEnd)
+        temp = 0.0
+        TDSNum = 0.0
+        ledauto(SetHourStart, SetMinuteStart, SetHourEnd, SetMinuteEnd)
         temp = TempAuto.tempauto(MinTemp, MaxTemp)
-        TDS.TDSRead()
+        TDSNum = TDS.TDSRead()
+        if temp == 0.0 or TDSNum == 0.0 :
+            water_status_db(temp,TDSNum)
         time.sleep(5)
--- dbUtils.py
+++ dbUtils.py
@@ -1,3 +1,28 @@
+import pymysql as mysql
+from datetime import datetime, time
 
-import mysql
 
+def water_status_db(temperature, tds):
+    db = mysql.connect(host="172.28.212.201", user="", password="", database="fishmanager",
+                       charset="utf8mb4")
+    # conn = MySQLd.connect("localhost", "root", "123456", "fish_know", charset="utf8mb4")
+    cursor = db.cursor()
+    # sql = "select * from water_status"
+    print(datetime.now())
+    sql = "INSERT INTO water_status(datatime,temp, tds) VALUES ('%s','%f','%f')" % (
+    datetime.now(), temperature, tds)
+    try:
+    # 执行sql语句
+        cursor.execute(sql)
+    # 执行sql语句
+        db.commit()
+    except:
+    # 发生错误时回滚
+        db.rollback()
+        print("bug")
+    # 关闭数据库连接
+    db.close()
+
+
+if __name__ == '__main__':
+    water_status_db(23, 400)
