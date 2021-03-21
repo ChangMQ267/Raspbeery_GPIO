63851a06d2068811e010f3e496535e34706c92ad
ChangMQ267 <ChangMQ267@163.com>
2021-03-21 18:18:33 +0800

    增加反复安装“systemctl daemon-reload”

--- install.sh
+++ install.sh
@@ -2,3 +2,4 @@ sudo sed -i 's/\r$//' ./fishManagement_start.sh
 sudo chmod +x fishManagement*
 sudo cp fishManagement_start.sh /usr/bin/
 sudo cp fishManagement.service /etc/systemd/system/
+sudo systemctl daemon-reload
